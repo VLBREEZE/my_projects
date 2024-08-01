4d58548516d96788e35ec93fd80bbd5b6ab892c7
VLBREEZE <166714949+VLBREEZE@users.noreply.github.com>
2024-08-01 11:19:13 +0900

    Add files via upload

---
+++ py_install/HBsirius_Lib/__init__.py
@@ -0,0 +1,2 @@
+from .HB_Lib import Sirius
+
---
+++ py_install/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='HB_Lib',
+    version='1.0.0',
+    author='Hong Team',
+    packages=find_packages(),
+)
+
+# cd py_install
+# sudo python3 setup.py install
